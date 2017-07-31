e7cf7cf0995c95b123050395aa1c753a6ad2913e
Ronnie <rpanaligan@ucmerced.edu>
2017-07-31 11:00:15 -0700

    Revert "trying out quick replies"
    This reverts commit 6184c0b94d3501286428153abe4fb95d9b6204c5.

--- app.js
+++ app.js
@@ -50,21 +50,7 @@ function sendMessage(recipientId, message) {
         method: 'POST',
         json: {
             recipient: {id: recipientId},
-            message:{
-              text:"Pick a color:",
-              quick_replies:[
-                {
-                  content_type:"text",
-                  title:"Red",
-                  payload:"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_RED"
-                },
-                {
-                  content_type:"text",
-                  title:"Green",
-                  payload:"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_GREEN"
-                }
-              ]
-            }
+            message: message,
         }
     }, function(error, response, body) {
         if (error) {
