5eaa6f3917afabe6e1da06c86d60804028795980
Ronnie <rpanaligan@ucmerced.edu>
2017-08-03 10:59:06 -0700

    3

--- app.js
+++ app.js
@@ -109,7 +109,7 @@ function sendMessage(recipientId, message0) {
             console.log('Error sending message: ', error);
         } else if (response.body.error) {
             console.log('Error: ', response.body.error);
-        } else if(state == 2 || state == 1){
+        } else if(state == 2){
           console.log(response.body);
           state = 0;
           sendMessage(recipientId, message);
